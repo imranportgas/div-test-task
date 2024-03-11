6e5e89150d65b60dd8bfc53f99712330f259a64d
Имран Псху <makerpolly4@gmail.com>
2024-03-11 12:55:02 +0300

    First commit. I made a table of requests and a model of requests.

---
+++ app/Models/Application.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $table = 'applications';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'status'
+    ];
+
+    public const STATUS_NOT_CONFIRMED = 0;
+    public const STATUS_CONFIRMED = 1;
+
+    public function statusLabels()
+    {
+        return [
+            self::STATUS_CONFIRMED => 'Resolved',
+            self::STATUS_NOT_CONFIRMED => 'Active'
+        ];
+    }
+}
