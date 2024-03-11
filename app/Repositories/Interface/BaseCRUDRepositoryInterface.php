51c0ec682ebca396a2014c15f7ecc408c85376cf
imran <imranpskhu@gmail.com>
2024-03-11 13:33:37 +0300

    Second commit. Создал базовый репозиторий реализующий методы интерфейса.

--- app/Repositories/Interface/BaseCRUDRepositoryInterface.php
+++ app/Repositories/Interface/BaseCRUDRepositoryInterface.php
@@ -3,10 +3,11 @@
 namespace App\Repositories\Interface;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 interface BaseCRUDRepositoryInterface
 {
     public function getAll(): Collection;
 
-    public function getById(int $id);
+    public function getById(int $id): ?Model;
 }
